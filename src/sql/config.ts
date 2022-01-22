3b278d764712d9a810ba43c9430fedfcf5d73ef3
Harry Xiong <harryxiong24@gmail.com>
2022-01-22 16:13:32 +0800

    feat: update config

--- src/sql/config.ts
+++ src/sql/config.ts
@@ -4,9 +4,9 @@ export const options: string | mysql.ConnectionConfig = {
   host: '182.92.221.129',
   // 可选，默认式3306
   port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'uwb',
+  user: '',
+  password: '',
+  database: '',
 };
 
 export default options;
